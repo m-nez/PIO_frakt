75e8d14c4f135a073c6a6254c0256a4611865065
Fiszcz <fiszczu@gmail.com>
2016-05-03 01:26:54 +0200

    Update renderer.cpp
    Dodanie trzech funkcji.

--- renderer.cpp
+++ renderer.cpp
@@ -44,9 +44,97 @@ void test_func(Plane* plane) {
 	}
 }
 
+void set_Mandelbrot(Plane* plane) {
+	int i, j, k;
+	double series[2];
+	double tmpserie;
+	double val;
+	double x, y;
+	double dx = (plane->right - plane->left) / (double)(plane->width); /* Left -> Right */
+	double dy = -(plane->up - plane->down) / (double)(plane->height); /* Up -> Down */
+
+	for(i = 0, y = plane->up; i < plane->height; ++i, y += dy) {
+		for(j = 0, x = plane->left; j < plane->width; j++, x += dx) {
+			series[0]=0;
+			series[1]=0;
+			for (k = 0; k < 10000; ++k) {
+				tmpserie = series[0]*series[0] - series[1]*series[1] + x;
+				series[1] = 2*series[0]*series[1] + y;
+				series[0] = tmpserie;
+				if ((series[0]*series[0] + series[1]*series[1]) > 4)
+					break;
+			}
+
+			val = (double)k/10000;
+
+			color( val, plane->data + (i*plane->width + j)*3);
+		}
+	}
+}
+
+void set_Julia(Plane* plane) {
+	int i, j, k;
+	double series[2];
+	double tmpserie;
+	double c[2] = {-0.123, 0.745};
+	double val;
+	double x, y;
+	double dx = (plane->right - plane->left) / plane->width; /* Left -> Right */
+	double dy = -(plane->up - plane->down) / plane->height; /* Up -> Down */
+
+	for(i = 0, y = plane->up; i < plane->height; ++i, y += dy) {
+		for(j = 0, x = plane->left; j < plane->width; j++, x += dx) {
+			series[0]=x;
+			series[1]=y;
+			for (k = 0; k < 1000; ++k) {
+				tmpserie = series[0]*series[0] - series[1]*series[1] + c[0];
+				series[1] = 2*series[0]*series[1] + c[1];
+				series[0] = tmpserie;
+				if ((series[0]*series[0] + series[1]*series[1]) > 4)
+					break;
+			}
+
+			val = (double)k/1000;
+
+			color( val, plane->data + (i*plane->width + j)*3);
+		}
+	}
+}
+
+void cubic_Julia(Plane* plane) {
+	int i, j, k;
+	double series[2];
+	double tmpserie;
+	double c[2] = {-0.123,0.745};
+	double val;
+	double x, y;
+	double dx = (plane->right - plane->left) / plane->width; /* Left -> Right */
+	double dy = -(plane->up - plane->down) / plane->height; /* Up -> Down */
+
+	for(i = 0, y = plane->up; i < plane->height; ++i, y += dy) {
+		for(j = 0, x = plane->left; j < plane->width; j++, x += dx) {
+			series[0]=x;
+			series[1]=y;
+			for (k = 0; k < 1000; ++k) {
+				tmpserie = series[0]*series[0]*series[0] - 3*series[0]*series[1]*series[1] + c[0];
+				series[1] = 3*series[0]*series[0]*series[1] - series[1]*series[1]*series[1] + c[1];
+				series[0] = tmpserie;
+				if ((series[0]*series[0] + series[1]*series[1]) > 4)
+					break;
+			}
+
+			val = (double)k/1000;
+
+			color( val, plane->data + (i*plane->width + j)*3);
+		}
+	}
+}
 
 static r_keys r_map[] = {
-	{"test", test_func}
+	{"test", test_func},
+	{"Mandelbrot", set_Mandelbrot},
+	{"Julia", set_Julia},
+	{"Cubic Julia", cubic_Julia}
 };
 
 #define NUM_RENDERERS (sizeof(r_map)/sizeof(r_keys))
