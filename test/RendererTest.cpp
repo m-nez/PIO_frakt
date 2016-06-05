f97db9dc070ccbc8f72d6db0feba8b8cf7b06560
m-nez <mn@openmailbox.org>
2016-06-05 13:34:15 +0200

    Fixed test error

--- test/RendererTest.cpp
+++ test/RendererTest.cpp
@@ -1,35 +1,31 @@
 #include <renderer.hpp>
 #include <gtest/gtest.h>
 
-namespace {
+TEST(RendererTest, Packaging) {
+	int count;
+	Plane plane(80,60);
+	Renderer* r = package_renderers(&plane, &count);
+	ASSERT_NE(nullptr, r);
+	EXPECT_NE(0, count);
 
-	TEST(RendererTest, Packaging) {
-		int count;
-		Plane plane(80,60);
-		Renderer* r = package_renderers(&plane, &count);
-		ASSERT_NE(nullptr, r);
-		EXPECT_NE(0, count);
-
-		int color_count;
-		c_keys* colors = package_colors(&color_count);
-		ASSERT_NE(nullptr, colors);
-		EXPECT_NE(0, color_count);
+	int color_count;
+	c_keys* colors = package_colors(&color_count);
+	ASSERT_NE(nullptr, colors);
+	EXPECT_NE(0, color_count);
 
 
-		for(int k = 0; k < color_count; ++k) {
-			plane.color = colors[k].function;
-			for(int j = 0; j < count; ++j) {
-				char nonZero = 0;
-				r[j].render();
-				for(int i = 0; i < plane.width * plane.height * 3; ++i) {
-					if(plane.data[i] != 0) {
-						nonZero = 1;
-						break;
-					}
+	for(int k = 0; k < color_count; ++k) {
+		plane.color = colors[k].function;
+		for(int j = 0; j < count; ++j) {
+			char nonZero = 0;
+			r[j].render();
+			for(int i = 0; i < plane.width * plane.height * 3; ++i) {
+				if(plane.data[i] != 0) {
+					nonZero = 1;
+					break;
 				}
-				EXPECT_EQ(1, nonZero) << r[j].get_id() << " " <<  colors[k].id;
 			}
+			EXPECT_EQ(1, nonZero) << r[j].get_id() << " " <<  colors[k].id;
 		}
 	}
-
 }
