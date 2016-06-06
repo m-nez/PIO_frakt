abb074e99f8f6869b3cb2402bc5e693f68db2255
achellko <biara.prime@gmail.com>
2016-06-06 20:53:07 +0200

    Update ColorTest.cpp

--- test/ColorTest.cpp
+++ test/ColorTest.cpp
@@ -2,9 +2,17 @@
 #include <renderer.hpp>
 
 TEST(ColorTest, greyscale) {
-
+	unsigned char* instance = new char[3];
+	double x = 0.1;
+	color_greyscale(x, instance);
+	unsigned char[3] result = {{25.5}, {25.5}, {25.5}};
+	EXPECT_EQ(result, instance);	
 }
 
 TEST(ColorTest, blue) {
-
+	unsigned char* instance = new char[3];
+	double x = 0.1;
+	color_blue(x, instance);
+	unsigned char[3] result = {{4.98}, {8.05}, {44.02}};
+	EXPECT_EQ(result, instance);
 }
